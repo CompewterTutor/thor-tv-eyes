aac1fcda47091fae493fb807a33c5a393655f3d8
Michael Morrissey <mike.d.morrissey@gmail.com>
2018-08-15 22:16:22 -0600

    compile problems

--- test.py
+++ test.py
@@ -6,20 +6,21 @@ import os
 
 
 
-
 def main():
 	#init pygame
 	pygame.init();
 	#load the logo
+	logo = pygame.image.load("res/logo.jpg")
+
 	eye_type = "defaultEye"
-	iris = pygame.image.load(eye_type + "/iris.png")
-	lid_lower = pygame.image.load(eye_type + "/lid-lower.png")
-	lid_lower_symmetrical = pygame.image.load(eye_type + "/lid-lower-symmetrical.png")
-	lid_upper = pygame.image.load(eye_type + "/lid-upper.png")
-	lid_upper_symmetrical = pygame.image.load(eye_type + "/lid-upper-symmetrical.png")
-	sclera = pygame.image.load(eye_type + "/sclera.png")
+	iris = pygame.image.load("res/eyes/" + eye_type + "/iris.png")
+	lid_lower = pygame.image.load("res/eyes/" + eye_type + "/lid-lower.png")
+	lid_lower_symmetrical = pygame.image.load("res/eyes/" + eye_type + "/lid-lower-symmetrical.png")
+	lid_upper = pygame.image.load("res/eyes/" + eye_type + "/lid-upper.png")
+	lid_upper_symmetrical = pygame.image.load("res/eyes/" + eye_type + "/lid-upper-symmetrical.png")
+	sclera = pygame.image.load("res/eyes/" + eye_type + "/sclera.png")
 	pygame.display.set_icon(logo)
-	pygame.display.pygame.display.set_caption("Thor Eyeball", icontitle=None)
+	pygame.display.set_caption("Thor Eyeball")
 
 	# create the screen
 	screen = pygame.display.set_mode((1920,1080))
